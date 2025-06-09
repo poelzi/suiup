352d8dbf60ef7e99ff94bb5923a8c2c34a7ed2cd
stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>
2025-06-09 09:09:56 -0700

    Update which to not print extra text (#48)

--- src/handlers/which.rs
+++ src/handlers/which.rs
@@ -7,7 +7,6 @@ use anyhow::Error;
 /// Handles the `which` command
 pub fn handle_which() -> Result<(), Error> {
     let default_bin = get_default_bin_dir();
-    println!("Default binaries path: {}", default_bin.display());
-    // Implement displaying the path to the active CLI binary here
+    println!("{}", default_bin.display());
     Ok(())
 }
